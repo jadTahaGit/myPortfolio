50cc734d9ce3824652b5419a7b0f5c5b1b9f3ceb
jadTahaGit <jadtaha.de@gmail.com>
2021-09-09 18:53:53 +0200

    Authority -- v-00.00.02

--- contact.php
+++ contact.php
@@ -2,10 +2,11 @@
 <html lang="en">
 
 <head>
+    <meta author="Jad Taha" />
+    <meta email="jadtaha@gmail.com" />
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
     <title>Jad Taha</title>
     <link rel="stylesheet" href="css/general.css" />
     <link rel="stylesheet" href="css/contact.css" />
